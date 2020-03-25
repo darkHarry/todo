454bc838c515fd1bef3e4c408ec09e453faa077d
darkHarry <harry.mecwan91@gmail.com>
2020-03-25 21:38:48 +0530

    Add autofocus to todo text field

--- project/todo/forms.py
+++ project/todo/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class AddTodoForm(forms.Form):
-    new_todo = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={"placeholder": "New Todo", "size": "40"}))
+    new_todo = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={"placeholder": "New Todo", "size": "40", "autofocus": "autofocus"}))
 
     def clean_new_todo(self):
         new_todo = self.cleaned_data["new_todo"]
